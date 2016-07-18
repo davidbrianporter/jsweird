6f96720d54ab04ed35f7b2fb0e966f517b5d8ff2
Porter <davidbrianporter@gmail.com>
2016-07-18 08:44:34 -0400

    is commit registering

--- lect20.js
+++ lect20.js
@@ -1,1 +1,8 @@
 // Lecture 20
+// Primitive Types - (not an object) - a SINGLE named value pair
+// 1 - undefined - lack of existence
+// 2 - null - set when you wnat value equal to nothing
+// 3 - bool
+// 4 - number - ***floating point ?
+// 5 - string
+// 6 - ***symbol ? - ES6
