dbed343de7ad45c7a991fa6a1e21c115dff408bd
Porter <davidbrianporter@gmail.com>
2016-07-20 10:05:48 -0400

    lect 35 function expressions and statements and memory

---
+++ lect35.js
@@ -0,0 +1,18 @@
+greet();
+
+function greet() {
+    console.log('hi');
+}
+
+anonymousGreet();
+
+var anonymousGreet = function() {
+    console.log('hi');
+}
+
+function log(a) {
+    console.log(a);
+}
+
+var b = 3;
+log(b);
