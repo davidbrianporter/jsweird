ca8130de5bc815975a88b1adeef9d711e0d16aef
Porter <davidbrianporter@gmail.com>
2016-07-23 21:13:08 -0400

    Reflection and Extend

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-var objectLiteral = {
-    firstName: "David",
-    isProgrammer: true
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;  
+    }
 }
 
-console.log(JSON.stringify(objectLiteral));
+var john = {
+    firstname: 'John',
+    lastname: 'Doe'
+}
 
-var jsonValue = JSON.parse('{ "firstName": "david", "isProgrammer": true }');
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
 
-console.log(jsonValue);
+for (var prop in john) {
+    if (john.hasOwnProperty(prop)) {
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+
+var jane = {
+    address: '111 Main St.',
+    getFormalFullName: function() {
+        return this.lastname + ', ' + this.firstname;   
+    }
+}
+
+var jim = {
+    getFirstName: function() {
+        return firstname;   
+    }
+}
+
+_.extend(john, jane, jim);
+
+console.log(john);
+
+
+
+
+
+
+
+
